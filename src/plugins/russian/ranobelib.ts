e2b65c6b88ed78f8e8961d3574a08549028bf43b
Rider21 <58046032+Rider21@users.noreply.github.com>
2024-10-31 10:02:51 +0300

    ooh

--- src/plugins/russian/ranobelib.ts
+++ src/plugins/russian/ranobelib.ts
@@ -18,11 +18,24 @@ class RLIB implements Plugin.PluginBase {
   name = 'RanobeLib';
   site = 'https://ranobelib.me';
   apiSite = 'https://api.lib.social/api/manga/';
-  version = '2.0.2';
+  version = '2.0.3';
   icon = 'src/ru/ranobelib/icon.png';
   imageRequestInit: Plugin.ImageRequestInit = {
     headers: {
-      Referer: this.site,
+      'sec-ch-ua':
+        '"Not)A;Brand";v="99", "Android WebView";v="127", "Chromium";v="127"',
+      'sec-ch-ua-mobile': '?1',
+      'user-agent':
+        'Mozilla/5.0 (Linux; Android 11; SM-A307FN Build/RP1A.200720.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/127.0.6533.103 Mobile Safari/537.36',
+      'sec-ch-ua-platform': 'Android',
+      'accept': 'image/avif,image/webp,image/apng,image/svg+xml,image/,/;q=0.8',
+      'sec-fetch-site': 'same-origin',
+      'sec-fetch-mode': 'no-cors',
+      'sec-fetch-dest': 'image',
+      'referer': this.site + '/',
+      'accept-encoding': 'gzip, deflate, br, zstd',
+      'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+      'priority': 'u=1, i',
     },
   };
   webStorageUtilized = true;
